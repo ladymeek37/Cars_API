094c87863baebb3a8c5ad96469e28c53e6fb366a
Lady Meek <ladyjmeek@gmail.com>
2022-12-21 12:56:08 -0600

    Created car model for car app + registered cars and rest framework in proj settings installed apps section

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# Models are representations of tables in the database.
+# "Code first approach" - create model and have application create table automatically based of class.
+
+class Car(models.Model):
+    make = models.CharField(max_length = 255)
+    model = models.CharField(max_length = 255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits = 8, decimal_places = 2)
+    
+
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Where we define all the functions the outside world can interact with through urls over the internet.
