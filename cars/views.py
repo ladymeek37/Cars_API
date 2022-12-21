f0e774de5a3fe914311a2e6306c6ad6917cc9a90
Lady Meek <ladyjmeek@gmail.com>
2022-12-21 13:44:42 -0600

    updated proj urls + created first view function + created cars app urls file

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
 # Where we define all the functions the outside world can interact with through urls over the internet.
+# Decorator is going to assign certian permissions to that function.
+# Here we will define what request types that function is capable of handling.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def cars_list(request):
+    
+
+    return Response('ok')
